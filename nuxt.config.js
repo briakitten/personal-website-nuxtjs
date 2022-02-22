d413361d98ee3f390ad6b79195274a1ec3b50fef
Bria Katrina Linza <brialinza@gmail.com>
2022-02-21 20:56:08 -0700

    Updated meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'website-v2',
+    title: 'Bria Katrina',
     htmlAttrs: {
       lang: 'en'
     },

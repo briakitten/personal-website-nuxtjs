c3b7cbdf0cfc71fbd8f2c3e998c3c7d788c56305
Bria Katrina Linza <brialinza@gmail.com>
2022-05-05 01:16:11 -0600

    Added TV art and link to heartrift

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,24 @@
 <template>
     <section>
-        <div class="columns">
-            <div class="column is-two-thirds">
+        <div class="columns pt-5 pb-5">
+            <div class="column is-half">
                 <h1 class="title">
                     Hello, I'm Bria!
                 </h1>
                 <p>
-                    I am a game programmer with experience in web development and all sorts of creative things.
-                    In my free time when I'm not programming, I make pixel art and music.
+                    I am a game programmer with experience in web development. I love science, space, VR...
+                    and weird niche indie games. Click on the TV to find what I've been working on.
                 </p>
+                <br><br>
+                <div class="columns">
+                    <div class="column">ヾ｜￣ー￣｜ﾉ</div>
+                    <div class="column">●__●</div>
+                </div>
             </div>
-            <div class="column">ヾ｜￣ー￣｜ﾉ</div>
-            <div class="column">●__●</div>
+            <div class="column">
+                <a class="image container is-256x256" href="https://heartrift.com/" target="_blank" rel="noopener noreferrer">
+                    <img src="~/assets/tv.png" >
+                </a></div>
         </div>
     </section>
 </template>

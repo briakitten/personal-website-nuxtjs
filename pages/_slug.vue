97dc7db52bef521c774389437737ff08bfc61269
Bria <brialinza@gmail.com>
2022-06-24 19:01:13 -0600

    fixed _slug style

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,18 +1,20 @@
 <template>
-    <article class="content">
-        <div class="card columns">
-            <div class="column p-4 pl-5">
-                <nuxt-link to="blog">
-                    <font-awesome-icon size="3x" :icon="['fas', 'chevron-left']"/>
-                </nuxt-link>
+    <section class="container px-6 mt-6 mb-6 is-max-desktop">
+        <article class="content">
+            <div class="card columns">
+                <div class="column p-4 pl-5">
+                    <nuxt-link to="blog">
+                        <font-awesome-icon size="3x" :icon="['fas', 'chevron-left']"/>
+                    </nuxt-link>
+                </div>
+                <div class="column is-11 p-4">
+                    <h1 style="margin-bottom: 0px;">{{article.title}}</h1>
+                    <span>{{article.date}}</span>
+                </div>
             </div>
-            <div class="column is-11 p-4">
-                <h1 style="margin-bottom: 0px;">{{article.title}}</h1>
-                <span>{{article.date}}</span>
-            </div>
-        </div>
-        <nuxt-content :document="article" />
-    </article>
+            <nuxt-content :document="article" />
+        </article>
+    </section>
 </template>
 
 <script>
